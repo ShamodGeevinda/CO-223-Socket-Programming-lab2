2d1ddd9bb4cdc5815f61e9cee6b29980dbc6f8a6
ShamodGeevinda <e18397@eng.pdn.ac.lk>
2021-09-30 11:59:33 +0530

    initial commit

---
+++ client.py
@@ -0,0 +1,19 @@
+import socket
+
+
+import socket
+msgFromClient = "Helllo UDP Server"
+bytesToSend = str.encode(msgFromClient)
+serverAddressPort = ("127.0.0.1",20001)
+buffersize = 1024
+
+# create UDP socket at client side
+UDPClientSocket = socket.socket(family=socket.AF_INET,type=socket.SOCK_DGRAM)
+
+# Send to server using created UDP socket
+
+UDPClientSocket.sendto(bytesToSend,serverAddressPort)
+msgFromServer = UDPClientSocket.recvfrom(buffersize)
+msg = "Message from server: "+msgFromServer[0].decode()
+
+print(msg)
